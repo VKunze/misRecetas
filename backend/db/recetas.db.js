e53c10186d5b0b75721bbe8262356c8b7e48ee92
valentinakunze <vkunze@roku.com>
2020-08-03 12:09:28 -0300

    To run locally

--- backend/db/recetas.db.js
+++ backend/db/recetas.db.js
@@ -1,24 +1,24 @@
-// module.exports = (sequelize, Sequelize) => {
-//     const Receta = sequelize.define("receta", {
-//       nombre: {
-//         type: Sequelize.STRING
-//       },
-//       tipoComida: {
-//         type: Sequelize.STRING
-//       },
-//       autor: {
-//         type: Sequelize.STRING
-//       },
-//       ingredientes: {
-//         type: Sequelize.STRING
-//       },
-//       descripcion: {
-//         type: Sequelize.STRING
-//       },
-//       imagen: {
-//         type: Sequelize.BLOB('long')
-//       }
-//     });
+module.exports = (sequelize, Sequelize) => {
+    const Receta = sequelize.define("receta", {
+      nombre: {
+        type: Sequelize.STRING
+      },
+      tipoComida: {
+        type: Sequelize.STRING
+      },
+      autor: {
+        type: Sequelize.STRING
+      },
+      ingredientes: {
+        type: Sequelize.STRING
+      },
+      descripcion: {
+        type: Sequelize.STRING
+      },
+      imagen: {
+        type: Sequelize.BLOB('long')
+      }
+    });
   
-//     return Receta;
-//   };
+    return Receta;
+  };
