ae997c768a6de4a60f99937b317cc2fbcace0728
VKunze <39528476+VKunze@users.noreply.github.com>
2020-08-04 11:55:45 -0300

    arreglos

--- app.js
+++ app.js
@@ -7,24 +7,24 @@ const http = require('http');
 var app = express();
 
 app.use(cors());
-// app.use(bodyParser.json({ limit: '5mb', extended: true }));
-// app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+app.use(bodyParser.json({ limit: '5mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 
 // all environments
 app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+//app.use(express.methodOverride());
+//app.use(app.router);
+//app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' === app.get('env')) {
+/* if ('development' === app.get('env')) {
   app.use(express.errorHandler());
-}
+} */
 
 db.sequelize.sync().then(() => {
   app.use(ruter);
